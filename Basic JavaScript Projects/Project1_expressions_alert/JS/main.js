509aaeebfa918b6b3b885a0a969dd94d6021e4cd
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2023-10-07 15:32:25 -0700

    Add assignment 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+let part1 = "This is a string ";
+let part2 = "with a quotation \" and backslash \\";
+
+// this expression concatenates the 2 parts together
+let fullString = part1 + part2;
+
+// write the string + a line break
+document.write(fullPart + "<br>");
+
+// show a window alert
+function window_alert() {
+    window.alert("Here is an alert!");
+}
